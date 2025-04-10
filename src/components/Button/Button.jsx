be2f295a838f14ba53ce16f5e7031275a71fa38f
Gustavo Faustino <32557780+GustavoFaustinoDeAzevedo@users.noreply.github.com>
2025-04-10 00:59:11 -0300

    Enhance Button component with animation on mouse up and update StyledButton styles for dynamic animations

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
-import StyledButton from '../../styles/components/StyledButton';
+import React, { useRef, useState } from "react";
+import StyledButton from "../../styles/components/StyledButton";
 
 const Button = ({ children, onClick, type, ariaLabel, variant }) => {
+  const [isAnimating, setIsAnimating] = useState(false);
+  const buttonRef = useRef(null);
+
+  const handleMouseUp = () => {
+    setIsAnimating(true);
+    setTimeout(() => setIsAnimating(false), 400); 
+  };
+
   return (
     <StyledButton
       onClick={onClick}
       type={type}
       aria-label={ariaLabel}
       $variant={variant}
+      $isAnimating={isAnimating}
+      ref={buttonRef}
+      onMouseUp={handleMouseUp}
     >
       {children}
     </StyledButton>
--- src/styles/components/StyledButton/StyledButton.jsx
+++ src/styles/components/StyledButton/StyledButton.jsx
@@ -1,18 +1,60 @@
 import { styled } from 'styletron-react';
 import variantStyles from './buttonStyles';
 
-const StyledButton = styled('button', ({ $variant = 'primary' }) => ({
-  padding: ' 0.8rem 2rem;',
-  border: 'none',
-  borderRadius: 'var(--border-radius)',
-  cursor: 'pointer',
-  fontWeight: '500',
-  transition: 'filter 300ms ease',
-  userSelect: 'none',
-  ...variantStyles[$variant],
-  ':hover': {
-    filter: 'brightness(140%)',
-  },
-}));
+const buttonPressedAnimation = (variant) => {
+  return {
+    '0%': {
+      // boxShadow: `0px 0px 0px 0px ${variantStyles[variant].backgroundColor}`,
+      outline: `1px double ${variantStyles[variant].backgroundColor}`,
+    },
+    '50%':{
+      outlineStyle: 'solid',
+    },
+    '100%': {
+      // boxShadow: '0px 0px 2px 10px #00000000',
+      outline: '10px double  #00000000'
+    },
+  };
+};
+
+// p.dotted {border-style: dotted;}
+// p.dashed {border-style: dashed;}
+// p.solid {border-style: solid;}
+// p.double {border-style: double;}
+// p.groove {border-style: groove;}
+// p.ridge {border-style: ridge;}
+// p.inset {border-style: inset;}
+// p.outset {border-style: outset;}
+// p.none {border-style: none;}
+// p.hidden {border-style: hidden;}
+// p.mix {border-style: dotted dashed solid double;}
+
+const StyledButton = styled(
+  'button',
+  ({ $variant = 'primary', $isAnimating }) => ({
+    width: '110px',
+    height: '30px',
+    border: 'none',
+    textAlign: 'center',
+    borderRadius: 'var(--border-radius)',
+    cursor: 'pointer',
+    fontWeight: '500',
+    transition: 'filter 300ms ease',
+    userSelect: 'none',
+    ...variantStyles[$variant], // Estilos de variante
+
+    ':hover': {
+      filter: 'brightness(140%)',
+    },
+    ':active': {
+      filter: 'brightness(60%)',
+    },
+
+    // Aplica os keyframes de animação dinamicamente
+    animationName: $isAnimating ? buttonPressedAnimation($variant) : 'none',
+    animationDuration: $isAnimating ? '0.37s' : 'none',
+    animationTimingFunction: 'ease-out',
+  })
+);
 
 export default StyledButton;
