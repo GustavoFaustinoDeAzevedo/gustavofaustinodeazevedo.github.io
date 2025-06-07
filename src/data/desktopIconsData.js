38431969ed429a79fbc093af18343c9a2f88c74d
Gustavo Faustino <32557780+GustavoFaustinoDeAzevedo@users.noreply.github.com>
2025-06-07 20:26:00 -0300

    Update Command Prompt title to indicate work in progress; comment out Tests entry

--- src/data/desktopIconsData.js
+++ src/data/desktopIconsData.js
@@ -4,11 +4,11 @@ export const desktopIconsData = [
   { id: 'skills', title: { eng: 'Skills', por: 'Habilidades' }, icon: 'icon skills' },
   { id: 'contact', title: { eng: 'Contact', por: 'Contato' }, icon: 'icon contact' },
   { id: 'change-background', title: { eng: 'Change Background', por: 'Alterar Fundo' }, icon: 'icon color-picker' },
-  { id: 'cmd', title: { eng: 'Command Prompt', por: 'Prompt de Comando' }, icon: 'icon command' },
+  { id: 'cmd', title: { eng: 'Command Prompt (Work in Progress)', por: 'Prompt de Comando (Em Desenvolvimento)' }, icon: 'icon command' },
   { id: 'task-manager', title: { eng: 'Task Manager (Work in Progress)', por: 'Gerenciador de Tarefas (Em Desenvolvimento)' }, icon: 'icon task-manager' },
   { id: 'browser', title: { eng: 'Browser', por: 'Navegador' }, icon: 'icon browser' },
   { id: 'github', title: { eng: 'Github', por: 'Github' }, icon: 'icon github', windowParams: { src: 'https://github.com/GustavoFaustinoDeAzevedo/gustavofaustinodeazevedo.github.io', title: { eng: 'Browser', por: 'Navegador' }, icon: 'icon browser' } },
-  { id: 'tests', title: { eng: 'Tests', por: 'Testes' }, icon: 'icon skills' },
+  //{ id: 'tests', title: { eng: 'Tests', por: 'Testes' }, icon: 'icon skills' },
 
   { id: 'new', title: { eng: 'New', por: 'Novo' }, icon: 'icon plus-circle' },
 
