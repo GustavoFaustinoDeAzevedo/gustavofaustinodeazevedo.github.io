919a094db196bfff2fda69cde4dbb51cfa9e09e0
Gustavo Faustino <32557780+GustavoFaustinoDeAzevedo@users.noreply.github.com>
2025-08-20 17:53:56 -0300

    chore: remoção temporária do componente de navegação web O componente foi removido temporariamente devido a restrições de segurança de alguns sites que bloqueiam o carregamento via iframe (uso de políticas como X-Frame-Options ou Content-Security-Policy). Irei fazer um simulador de navegador futuramente com sites fictícios

--- src/App/hooks/useIsMobile.ts
+++ src/App/hooks/useIsMobile.ts
@@ -4,9 +4,10 @@ const useIsMobile = (): boolean => {
   const [isMobile, setIsMobile] = useState<boolean>(false);
 
   useLayoutEffect(() => {
-    const check = () => {
-      const screenCheck = window.matchMedia('(max-width: 768px)').matches;
-      const userAgentCheck =
+    const check = (): void => {
+      const screenCheck: boolean =
+        window.matchMedia('(max-width: 768px)').matches;
+      const userAgentCheck: boolean =
         /android|iphone|ipad|ipod|blackberry|windows phone/i.test(
           navigator.userAgent
         );
