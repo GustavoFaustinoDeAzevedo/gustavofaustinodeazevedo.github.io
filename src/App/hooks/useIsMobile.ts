ff13c8a186800f480e207fbefbf32a14a9d47af3
Gustavo Faustino de Azevêdo <gustavofaustino18@hotmail.com>
2025-08-20 17:24:09 -0300

    feat: adicionado hook para detectar se o usuário está em um dispositivo mobile

--- src/App/hooks/useIsMobile.ts
+++ src/App/hooks/useIsMobile.ts
@@ -1,12 +1,12 @@
 import { useLayoutEffect, useState } from 'react';
 
-const useIsMobile = () => {
-  const [isMobile, setIsMobile] = useState(false);
+const useIsMobile = (): boolean => {
+  const [isMobile, setIsMobile] = useState<boolean>(false);
 
   useLayoutEffect(() => {
-    const check = () => {
-      const screenCheck = window.matchMedia('(max-width: 768px)').matches;
-      const userAgentCheck =
+    const check = (): void => {
+      const screenCheck: boolean = window.matchMedia('(max-width: 768px)').matches;
+      const userAgentCheck: boolean =
         /android|iphone|ipad|ipod|blackberry|windows phone/i.test(
           navigator.userAgent
         );
