dda9597e7aeb3c1183d4eb43e96210bfacaa7252
Gustavo Faustino <32557780+GustavoFaustinoDeAzevedo@users.noreply.github.com>
2025-09-11 21:15:56 -0300

    bugfix: corrige versão mobile.

--- src/App/hooks/useIsMobile.ts
+++ src/App/hooks/useIsMobile.ts
@@ -7,16 +7,19 @@ const useIsMobile = (): boolean => {
     const check = (): void => {
       const screenCheck: boolean =
         window.matchMedia('(max-width: 768px)').matches;
+      console.log('screenCheck', screenCheck);
       const userAgentCheck: boolean =
         /android|iphone|ipad|ipod|blackberry|windows phone/i.test(
           navigator.userAgent
         );
+      console.log('userAgentCheck', userAgentCheck);
       setIsMobile(screenCheck || userAgentCheck);
     };
 
     check();
   }, []);
 
+  console.log('isMobile', isMobile);
   return isMobile;
 };
 
