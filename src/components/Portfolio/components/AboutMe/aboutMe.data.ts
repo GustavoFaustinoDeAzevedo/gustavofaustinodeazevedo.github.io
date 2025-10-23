e5f54c9329c1ae2e37e3a2284ba92d270ba08cce
Gustavo Faustino <32557780+GustavoFaustinoDeAzevedo@users.noreply.github.com>
2025-10-23 17:51:30 -0300

    style: Ajusta estilo do portifólio para facilitar refatoração futura.

--- src/components/Portfolio/components/AboutMe/aboutMe.data.ts
+++ src/components/Portfolio/components/AboutMe/aboutMe.data.ts
@@ -15,12 +15,12 @@ const aboutMeData: AboutMeData = {
   por: {
     title: 'Gustavo Faustino de Azevêdo',
     subtitle: 'Desenvolvedor Web React/JS | Especialista em HTML/CSS.',
-    text: 'Graduado em Engenharia da Computação, sou apaixonado por criar interfaces intuitivas e visualmente marcantes com HTML, CSS e React — especialmente em temas escuros. Estou em busca de oportunidades como desenvolvedor front-end para crescer profissionalmente e colaborar em projetos que entreguem experiências eficientes e bem planejadas aos usuários.',
+    text: 'Graduado em Engenharia da Computação, sou apaixonado por criar interfaces intuitivas e visualmente marcantes com HTML, CSS e React. Estou em busca de oportunidades como desenvolvedor front-end para crescer profissionalmente e colaborar em projetos que entreguem experiências eficientes e bem planejadas aos usuários.',
   },
   eng: {
     title: 'Gustavo Faustino de Azevêdo',
     subtitle: 'React/JS Web Developer | HTML/CSS expertise.',
-    text: "I hold a degree in Computer Engineering and have a passion for crafting intuitive and visually striking interfaces using HTML, CSS, and React — especially with dark themes. I'm seeking opportunities as a front-end developer to grow professionally and contribute to projects that deliver efficient and well-designed user experiences.",
+    text: "I hold a degree in Computer Engineering and have a passion for crafting intuitive and visually striking interfaces using HTML, CSS, and React. I'm seeking opportunities as a front-end developer to grow professionally and contribute to projects that deliver efficient and well-designed user experiences.",
   },
 };
 
--- src/components/Portfolio/components/SendMessage/SendMessage.tsx
+++ src/components/Portfolio/components/SendMessage/SendMessage.tsx
@@ -28,37 +28,39 @@ const SendMessage = ({ language }: { language: Language }) => {
   ];
 
   return (
-    <form
-      className="contact-form"
-      aria-label={formDataTranslated.form.ariaLabel}
-      action="https://formspree.io/f/mnqelzyz"
-      method="POST"
-      target="placeholder"
-    >
-      <h2 className="txt-weight-600 txt-center margin-bottom-1">
-        {formDataTranslated.title}
-      </h2>
-      {fields.map(({ id, type, name, config, textArea }) => (
-        <AnimatedInput
-          key={id}
-          id={id}
-          type={type}
-          name={name}
-          label={config.label}
-          required
-          ariaLabel={config.ariaLabel}
-          inputPlaceholder={config.placeholder}
-          textArea={textArea}
-        />
-      ))}
-      <Button
-        type="submit"
-        ariaLabel={formDataTranslated.button.ariaLabel}
-        variant={'primary'}
+    <div className="send-message">
+      <form
+        className="send-message__form"
+        aria-label={formDataTranslated.form.ariaLabel}
+        action="https://formspree.io/f/mnqelzyz"
+        method="POST"
+        target="placeholder"
       >
-        {formDataTranslated.button.label}
-      </Button>
-    </form>
+        <h2 className="text-weight-600 text-center margin-bottom-1 ">
+          {formDataTranslated.title}
+        </h2>
+        {fields.map(({ id, type, name, config, textArea }) => (
+          <AnimatedInput
+            key={id}
+            id={id}
+            type={type}
+            name={name}
+            label={config.label}
+            required
+            ariaLabel={config.ariaLabel}
+            inputPlaceholder={config.placeholder}
+            textArea={textArea}
+          />
+        ))}
+        <Button
+          type="submit" 
+          ariaLabel={formDataTranslated.button.ariaLabel}
+          variant={'primary'}
+        >
+          {formDataTranslated.button.label}
+        </Button>
+      </form>
+    </div>
   );
 };
 
