aeebbfd98f401cab513a8e625052a33c9a36e8ea
Gustavo Faustino <32557780+GustavoFaustinoDeAzevedo@users.noreply.github.com>
2025-11-28 09:47:08 -0300

    refactor(nodeIndex, StartMenu): remove arquivo obsoleto de indexação de nós; modificação sutil no estilo do ícone para limpeza do input

--- src/store/utils/updateStateIfDefined.ts
+++ src/store/utils/updateStateIfDefined.ts
@@ -1,12 +1,24 @@
+/**
+ * Atualiza um objeto de estado com atualizações parciais.
+ * @param state - O objeto de estado a ser atualizado.
+ * @param updates - Atualizações parciais a serem aplicadas ao objeto de estado.
+ * @returns Objeto de estado atualizado.
+ * @template T - O tipo do objeto de estado.
+ */
+
 const updateStateIfDefined = <T extends Record<string, unknown>>(
   state: T,
   updates: Partial<T>
 ): T => {
+
+  const definedUpdates = Object.fromEntries(
+    Object.entries(updates).filter(([_, value]) => value !== undefined)
+  ) as T;
+
+
   return {
     ...state,
-    ...(Object.fromEntries(
-      Object.entries(updates).filter(([_, value]) => value !== undefined)
-    ) as T),
+    ...definedUpdates,
   };
 };
 
