7c66206f2178c620d65b1658786b86c9450f2577
Gustavo Faustino <32557780+GustavoFaustinoDeAzevedo@users.noreply.github.com>
2026-02-24 20:20:09 -0300

    refactor(AnimatedInput): ajuste de animação da label do input
    - Animação da label ajustada para não ocorrer flash inicial
    - Converte o componente AnimatedInput para TypeScript
    - Otimiza o componente AnimatedInput para não redenrizar desnecessáriamente

--- src/components/Portfolio/components/SendMessage/SendMessage.tsx
+++ src/components/Portfolio/components/SendMessage/SendMessage.tsx
@@ -1,34 +1,55 @@
 import Button from '@components/ui/Button';
 import AnimatedInput from '@window/components/AnimatedInput';
-import { Language } from '@/store/slices/settings';
 import formData from './SendMessage.data';
 import { useSelector } from 'react-redux';
 import { RootState } from '@/store';
+import React, { useMemo } from 'react';
 
 const SendMessage = () => {
   const language = useSelector((state: RootState) => state.settings.language);
-  const formDataTranslated = formData[language];
-  const fields = [
-    {
-      id: 'name',
-      type: 'input',
-      name: 'Name',
-      config: formDataTranslated.name,
-    },
-    {
-      id: 'email',
-      type: 'email',
-      name: 'Email',
-      config: formDataTranslated.email,
-    },
-    {
-      id: 'message',
-      type: 'text',
-      name: 'Message',
-      config: formDataTranslated.message,
-      textArea: true,
-    },
-  ];
+  const formDataTranslated = useMemo(() => formData[language], [language]);
+  const fields = useMemo(
+    () => [
+      {
+        id: 'name',
+        type: 'input',
+        name: 'Name',
+        config: formDataTranslated.name,
+      },
+      {
+        id: 'email',
+        type: 'email',
+        name: 'Email',
+        config: formDataTranslated.email,
+      },
+      {
+        id: 'message',
+        type: 'text',
+        name: 'Message',
+        config: formDataTranslated.message,
+        textArea: true,
+      },
+    ],
+    [formDataTranslated],
+  );
+
+  const mappedFields = useMemo(
+    () =>
+      fields.map(({ id, type, name, config, textArea }) => (
+        <AnimatedInput
+          key={id}
+          id={id}
+          type={type}
+          name={name}
+          label={config.label}
+          required
+          ariaLabel={config.ariaLabel}
+          inputPlaceholder={config.placeholder}
+          textArea={textArea}
+        />
+      )),
+    [formDataTranslated],
+  );
 
   return (
     <div className="send-message">
@@ -42,19 +63,7 @@ const SendMessage = () => {
         <h2 className="text-weight-600 text-center margin-bottom-1 ">
           {formDataTranslated.title}
         </h2>
-        {fields.map(({ id, type, name, config, textArea }) => (
-          <AnimatedInput
-            key={id}
-            id={id}
-            type={type}
-            name={name}
-            label={config.label}
-            required
-            ariaLabel={config.ariaLabel}
-            inputPlaceholder={config.placeholder}
-            textArea={textArea}
-          />
-        ))}
+        {mappedFields}
         <Button
           type="submit"
           ariaLabel={formDataTranslated.button.ariaLabel}
