6600074707e09ecd17db45041a02111ca958a313
Gustavo Faustino <32557780+GustavoFaustinoDeAzevedo@users.noreply.github.com>
2026-04-03 17:13:07 -0300

    feat(simplifiedPortfolio): Introduz uma nova versão do portfólio
    - Introduz uma nova versão da página para, quando for implementado, mostrar apenas o portfólio se o usuário preferir.

--- src/components/Portfolio/components/AboutMe/AboutMe.tsx
+++ src/components/Portfolio/components/AboutMe/AboutMe.tsx
@@ -2,31 +2,57 @@ import { useSelector } from 'react-redux';
 import aboutMeData, { AboutMeData } from './aboutMe.data';
 import { RootState } from '@/store';
 
-const AboutMe = () => {
+interface AboutMeProps {
+  classContainer?: string;
+  classWrapper?: string;
+  classImage?: string;
+  classTitleContainer?: string;
+  classTitle?: string;
+  classSubtitle?: string;
+  classHorizontalRule?: string;
+  classText?: string;
+  text?: string;
+  image?: string;
+  imageAlt?: string;
+  title?: string;
+  subtitle?: string;
+}
+
+const AboutMe = (props: AboutMeProps) => {
   const language = useSelector((state: RootState) => state.settings.language);
   return (
     <main
-      className="about-me"
+      className={props.classContainer || 'about-me'}
       aria-label="About Me Section"
       data-initial-dimension='{"width": "500px", "height": "550px"}'
     >
-      <div className="about-me__wrapper">
+      <div className={props.classWrapper || 'about-me__wrapper'}>
         <img
-          src="images/profile-pic.png"
-          alt="A profile picture of Gustavo Faustino de Azevedo"
+          src={props.image || 'images/profile-pic.png'}
+          alt={props.imageAlt || aboutMeData[language].imageAlt}
           loading="lazy"
           decoding="async"
           referrerPolicy="no-referrer"
           fetchPriority="high"
-          className="about-me__image"
+          className={props.classImage || 'about-me__image'}
         />
 
-        <section className="about-me__title">
-          <h2>{aboutMeData[language].title}</h2>
-          <h3>{aboutMeData[language].subtitle}</h3>
-          <hr />
+        <section
+          className={props.classTitleContainer || 'about-me__title-container'}
+        >
+          <h2 className={props.classTitle || 'about-me__title'}>
+            {props.title || aboutMeData[language].title}
+          </h2>
+          <h3 className={props.classSubtitle || 'about-me__subtitle'}>
+            {aboutMeData[language].subtitle}
+          </h3>
+          <hr
+            className={props.classHorizontalRule || 'about-me__horizontal-rule'}
+          />
         </section>
-        <p className="about-me__text">{aboutMeData[language].text}</p>
+        <p className={props.classText || 'about-me__text'}>
+          {props.text || aboutMeData[language].text}
+        </p>
       </div>
     </main>
   );
--- src/components/Portfolio/components/AboutMe/aboutMe.data.ts
+++ src/components/Portfolio/components/AboutMe/aboutMe.data.ts
@@ -1,10 +1,12 @@
 export interface AboutMeData {
   por: {
+    imageAlt: string;
     title: string;
     subtitle: string;
     text: string;
   };
   eng: {
+    imageAlt: string;
     title: string;
     subtitle: string;
     text: string;
@@ -13,11 +15,13 @@ export interface AboutMeData {
 
 const aboutMeData: AboutMeData = {
   por: {
+    imageAlt: 'Foto de perfil de Gustavo Faustino de Azevedo',
     title: 'Gustavo Faustino de Azevêdo',
     subtitle: 'Desenvolvedor Web React/JS | Especialista em HTML/CSS.',
     text: 'Graduado em Engenharia da Computação, sou apaixonado por criar interfaces intuitivas e visualmente marcantes com HTML, CSS e React. Estou em busca de oportunidades como desenvolvedor front-end para crescer profissionalmente e colaborar em projetos que entreguem experiências eficientes e bem planejadas aos usuários.',
   },
   eng: {
+    imageAlt: 'A profile picture of Gustavo Faustino de Azevedo',
     title: 'Gustavo Faustino de Azevêdo',
     subtitle: 'React/JS Web Developer | HTML/CSS expertise.',
     text: "I hold a degree in Computer Engineering and have a passion for crafting intuitive and visually striking interfaces using HTML, CSS, and React. I'm seeking opportunities as a front-end developer to grow professionally and contribute to projects that deliver efficient and well-designed user experiences.",
