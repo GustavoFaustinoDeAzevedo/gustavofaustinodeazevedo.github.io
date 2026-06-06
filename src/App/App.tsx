0b932bcd31221a722f0ff6d6c30542151b424702
Gustavo Faustino <32557780+GustavoFaustinoDeAzevedo@users.noreply.github.com>
2026-06-06 13:54:33 -0300

    feat(pages): criar pagina NotFound e rotas
    - Criada página NotFound para rotas inválidas
    - Configurada rota principal para o Kernel

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,15 +1,16 @@
-import { Kernel } from '@GustavOS';
-import { Portfolio } from '@portfolio';
+import { NotFound, Portfolio, Kernel } from '@/pages';
+
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
     <Routes>
+      <Route path="/" element={<Kernel />} />
+      {/* <Route path="/" element={<Portfolio.FullVersionPortfolio />} /> */}
       <Route path="/portfolio" element={<Portfolio.FullVersionPortfolio />} />
       <Route path="/gustavos" element={<Kernel />} />
-      <Route path="/" element={<div>Home</div>} />
-      <Route path="*" element={<div>404</div>} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
